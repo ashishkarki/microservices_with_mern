5439a9837bcef045c1509f229cc325c06e17ad9c
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-01-17 14:40:56 +0545

    feat: adding logic to peeps-service but query service and some react frontend components yet to be implemented

--- Peeps-Service/src/index.ts
+++ Peeps-Service/src/index.ts
@@ -1,6 +1,30 @@
 import express from 'express'
 import moongoose from 'mongoose'
+import PeepModel from './models/peep'
 import router from './routes/routes'
+
+const amqp = require('amqplib')
+
+async function processSquawkMsg(msg: { content: { toString: () => any } }) {
+  const content = JSON.parse(msg.content.toString())
+  const { squawkId } = content
+  console.log(
+    `Peeps service - processSquawkMsg - squawkId: ${squawkId}, content: ${content}`,
+  )
+
+  const peepData = {
+    squawkId,
+    peeps: [],
+  }
+
+  try {
+    const newPeep = new PeepModel(peepData)
+    await newPeep.save()
+    console.log(`Peep ${squawkId} added to squawk ${squawkId}`)
+  } catch (error) {
+    console.log(`Peeps service - processSquawkMsg - error: ${error}`)
+  }
+}
 
 const app = express()
 
@@ -9,16 +33,45 @@ app.use(router)
 
 const Startup = async () => {
   try {
-    await moongoose.connect(
-      'mongodb://birdsquawk-mongo-service:27017/birdsquawk',
+    await moongoose.connect('mongodb://peep-mongo-service:27017/peep')
+    console.log('peeps-Service - index.ts - Connected to MongoDB')
+
+    const connection = await amqp.connect(
+      'amqp://rabbitmq-service:5672',
+      'heartbeat-30',
     )
-    console.log('BS-Service - index.ts - Connected to MongoDB')
+    console.log('peeps-Service - index.ts - Connected to RabbitMQ')
+
+    const channel = await connection.createChannel()
+    await channel.assertExchange('birdsquawk-exchange', 'topic', {
+      durable: false,
+    })
+    console.log('peeps-Service - index.ts - Exchange created')
+
+    await channel.assertQueue('peeps-squawk-queue', {
+      durable: false,
+    })
+    await channel.bindQueue(
+      'peeps-squawk-queue',
+      'birdsquawk-exchange',
+      'squawk.#',
+    )
+    await channel.consume(
+      'peeps-squawk-queue',
+      async (msg: { content: { toString: () => any } }) => {
+        console.log('Peeps service - index.ts - message received')
+
+        await processSquawkMsg(msg)
+        await channel.ack(msg)
+      },
+      { noAck: false },
+    )
   } catch (error) {
-    console.error('BS-Service - index.ts error!!! ' + error)
+    console.error('Peeps service - index.ts error!!! ' + error)
   }
 
-  app.listen(5050, () => {
-    console.log('BirdSquawk Service is running on port 5050')
+  app.listen(5051, () => {
+    console.log('BirdSquawk Service is running on port 5051')
   })
 }
 
--- Peeps-Service/src/routes/routes.ts
+++ Peeps-Service/src/routes/routes.ts
@@ -1,13 +1,66 @@
 import express, { Request, Response } from 'express'
+import { Mongoose } from 'mongoose'
+
+import PeepModel from '../models/peep'
+
+// import few things
+const ampq = require('amqplib')
+const { randomBytes } = require('crypto')
 
 // router
 const router = express.Router()
 
 // paths
-router.get('/api/v1/birdsquawk/get', (req: Request, res: Response) => {
-  res.send('BirdSquawk Service is running on port 5050')
+router.get('/api/v1/peeps/:squawkId', (req: Request, res: Response) => {
+  const squawkId = req.params.squawkId
+
+  PeepModel.find({ squawkId }, (err, peeps) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+    res.send(peeps)
+  })
 })
 
-router.post('/api/v1/birdsquawk/post', (req: Request, res: Response) => {})
+router.post('/api/v1/peeps/:squawkId', async (req: Request, res: Response) => {
+  const { peep } = req.body
+  const peepid = randomBytes(4).toString('hex')
+  const squawkId = req.params.squawkId
+  const peepData = {
+    peepid,
+    peep,
+  }
+  const msg = {
+    peepid,
+    peep,
+    squawkId,
+  }
+
+  try {
+    await PeepModel.updateOne({ squawkId }, { $push: { peeps: peepData } })
+    console.log(`Peep ${peepid} added to squawk ${squawkId}`)
+
+    // send message to queue
+    const connection = await ampq.connect('amqp://rabbitmq-service:5672')
+    console.log(`peeps api connected to rabbitmq`)
+
+    const channel = await connection.createChannel()
+    await channel.assertExchange('birdsquawk-exchange', 'topic', {
+      durable: false,
+    })
+    console.log(`peeps api exchange created`)
+
+    await channel.publish(
+      'birdsquawk-exchange',
+      'peep',
+      Buffer.from(JSON.stringify(msg)),
+    )
+    console.log(`peeps api message published`)
+
+    res.status(201).send(peepData)
+  } catch (error) {
+    res.status(500).send('Peeps service routes - error:' + error)
+  }
+})
 
 export default router
